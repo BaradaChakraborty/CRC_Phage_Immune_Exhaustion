ad9dd9fff9d7d814300829339ecd5af6f86b5f43
Barada Chakraborty <baradalmp@gmail.com>
2026-03-06 09:31:35 +0530

    feat: construct ASV table and remove PCR chimeras

--- scripts/03_sample_inference.R
+++ scripts/03_sample_inference.R
@@ -9,3 +9,13 @@ print(dadaFs[[1]])
 mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
 # Inspect the merged data frame from the very first sample
 head(mergers[[1]])
+# STEP 7: CONSTRUCT SEQUENCE TABLE & REMOVE CHIMERAS
+# This creates a matrix: Rows are Samples, Columns are unique DNA sequences
+seqtab <- makeSequenceTable(mergers)
+dim(seqtab)
+# Remove Chimeras (PCR artifacts)
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE, verbose=TRUE)
+
+# Calculate the percentage of our data that was biologically real (non-chimeric)
+real_proportion <- sum(seqtab.nochim) / sum(seqtab)
+print(paste("Proportion of clean sequences retained:", round(real_proportion * 100, 2), "%"))
