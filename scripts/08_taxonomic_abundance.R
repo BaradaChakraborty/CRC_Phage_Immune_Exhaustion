9459f4dff3746b1a0f601b9c8e2c50470128691c
Barada Chakraborty <baradalmp@gmail.com>
2026-03-08 21:19:38 +0530

    chore: format and clean script 08 for production

--- scripts/08_taxonomic_abundance.R
+++ scripts/08_taxonomic_abundance.R
@@ -1,12 +1,9 @@
-# STEP 12: TAXONOMIC COMPOSITION (RELATIVE ABUNDANCE)
 library(phyloseq); library(ggplot2)
 
 physeq_rel <- transform_sample_counts(physeq_obj, function(x) x / sum(x))
 
 physeq_phylum <- tax_glom(physeq_rel, taxrank = "Phylum")
 
-# Create the Stacked Bar Plot
-# We put 'Day' on the X-axis and fill the bars with the 'Phylum' colors
 abundance_plot <- plot_bar(physeq_phylum, x="Day", fill="Phylum") +
   geom_bar(stat="identity", color="black", size=0.1) + # Adds clean black outlines to the segments
   theme_bw() +
@@ -16,12 +13,10 @@ abundance_plot <- plot_bar(physeq_phylum, x="Day", fill="Phylum") +
        x = "Clinical Timepoint (Day)",
        y = "Relative Abundance (100%)")
 
-# Save the plot to your figures directory
 ggsave(filename = "figures/relative_abundance_phylum.png", 
        plot = abundance_plot, 
        width = 10, 
        height = 6, 
        dpi = 300)
 
-# Display the plot
 abundance_plot
